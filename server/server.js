117e52e175ca50381d4aa4bd7fd50497e8f56f16
Chris Rune <gtrabbit@gmail.com>
2017-08-29 20:20:05 -0400

    adding git ignore

--- server/server.js
+++ server/server.js
@@ -1,3 +1,6 @@
+/* jshint node: true */
+'use strict';
+
 //basics
 const express = require('express');
 const app = express();
@@ -51,7 +54,7 @@ let transporter = nodemailer.createTransport({
 		user: 'chrisrunemailer@gmail.com',
 		pass: 'turtle123'
 	}
-})
+});
 
 
 
@@ -62,15 +65,15 @@ const client_secret = 'srPUwdZ9qE0BNQGOfePxuSJrDmPh9jQdZBR5oX8QtsLWXDjjQATihDMsw
 let yelpClient;
 //creates a yelp client with the auth token
 yelp.accessToken(client_id, client_secret).then(response =>{
-	yelpClient = yelp.client(response.jsonBody.access_token)
-}).catch(e =>{
-
+	yelpClient = yelp.client(response.jsonBody.access_token);
+}).catch( e => {
+	console.log(e);
 });
 
 app.get('/map', function(req, res){
 	app.use('/assets', express.static(path.join(__dirname, '/../map/assets/')));
 	res.sendFile(path.join(__dirname + '/../map/index.html'));
-})
+});
 
 app.get('/yelp/:name/:loc', function(req, res){
 	yelpClient.search({
@@ -80,19 +83,19 @@ app.get('/yelp/:name/:loc', function(req, res){
 
 	}).then(response =>{
 		// yelpClient.
-		res.send(response.jsonBody.businesses[0])
+		res.send(response.jsonBody.businesses[0]);
 	}).catch(e => {
   		console.log(e);
 	});
 
-})
+});
 
 
 
 app.get('/', function(req, res){
 	app.use(express.static(__dirname + '/../portfolio/'));
 	res.sendFile(path.join(__dirname + '/../portfolio/html/index.html'));
-})
+});
 
 
 app.post('/sendemail', jsonParser, function(req, res){
@@ -102,33 +105,25 @@ app.post('/sendemail', jsonParser, function(req, res){
 		to: 'gtrabbit@gmail.com',
 		subject: 'new message from ' + req.body.name + ' sent from your contact form',
 		html: '<p>' + req.body.name + '(' + req.body.email + ")" + ' sent you the following message: </p>' + '<p>' + req.body.msg + '</p>'
-	}
-	transporter.sendMail(mailOptions, (err, info) =>{
+	};
+	transporter.sendMail(mailOptions, (err) =>{
 		if (err){
 			res.send(err);
 		} else {
 			res.send('Thank you for your response! The message was successfully delivered.');
 		}
 	
-	})
+	});
 
 
-})
-
-
-
-
-
-
-
-
+});
 
 
 //listening....
 server.listen(port, function(){
-	let host = server.address().address
-	let port = server.address().port
+	let host = server.address().address;
+	let port = server.address().port;
 	console.log(host);
 	console.log('listening on http://%s:%s', host, port);
-})
+});
 
