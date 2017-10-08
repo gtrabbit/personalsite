2b81f17bbf24f94b851fdd813e6cc37e0a9434f4
Chris Rune <gtrabbit@gmail.com>
2017-10-07 23:55:17 -0400

    feat: rebuild of portfolio
    Moving all projects to their own routes. Simplifying design.

--- portfolio/js/scripts.js
+++ portfolio/js/scripts.js
@@ -6,15 +6,7 @@ window.onload = function(){
 
 
 const makeEventListeners = function(){
-	//scroll one page
-	document.getElementById('scrollButton').addEventListener('click', function(){
-		for (let i = 0; i<=20; i++){
-			window.scrollTo(0, 0);
-			window.setTimeout(function(){
-				window.scrollBy(0, window.innerHeight/20);
-			}, i*i);
-		}
-	})
+
 
 	//adds listeners to navigation
 	Array.from(document.getElementsByClassName('navLink')).forEach(function(a){
@@ -80,34 +72,22 @@ const smoothScroll = function(elID){
 
 
 const makeScrolly = function(){
-	let height = window.innerHeight;
 	let faders = Array.from(document.getElementsByClassName('fader'));
 	let paraEls = Array.from(document.getElementsByClassName('parallax'));
+
 	window.onscroll = ()=>{
+		console.log("scroll happens")
 		faders.forEach(function(a){
 	 		a.style.opacity =  1 - Math.pow( Math.abs( ( ( (window.innerHeight - (a.scrollHeight  ) )  /2) - (a.offsetTop - window.scrollY ) ) / (window.innerHeight/2)  ), 3 )
 	 		})
-		paraEls.forEach(el=>{
+		paraEls.forEach((el) => {
+			value = (window.pageYOffset - el.offsetTop + window.innerHeight)
+			let yPos = -(value / 8);
+			let coords = '50% ' + yPos + 'px';
+			//console.log(coords)
+			el.style.backgroundPosition = coords;
+		})	
 
-			if ((document.body.scrollTop  > el.offsetTop - window.innerHeight) 
-				&& (document.body.scrollTop  < (el.offsetTop + el.scrollHeight)) ){
-					let value = (el.offsetTop - document.body.scrollTop) / window.innerHeight / 4  //makes a number from 0.25 to -1
-					let paraOffset = -(value * (window.innerHeight / 12) );
-					let coords = "50% " + (paraOffset) + '%';
-					el.style.backgroundPosition = coords;
-					let colorValue;
-					if (el.dataset.hasOwnProperty('first') && el.dataset.first === "true"){
-						colorValue = Math.round((255 * .75) + (255 * value)) + 65;
-					} else {
-						colorValue = Math.round((255 * .75) + (255 * value));
-					}
-					if (el.dataset.hasOwnProperty('inverse') && el.dataset.inverse === "true"){
-						colorValue = 255 - colorValue;
-					}
-					let color = "rgb(" + colorValue + ", " + colorValue + ", " + colorValue + ")";
-					el.style.backgroundColor = color;
-			}
-		})
 	}
 	
 }
